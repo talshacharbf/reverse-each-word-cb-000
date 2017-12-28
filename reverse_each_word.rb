af8e62666e7ca78da9e8e04d76d3a3c7d69c5dbe
tal shachar <tal.shachar@buzzfeed.com>
2017-12-28 06:24:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-newsentence = []
-newsentence = sentence.split(" ")
+  newsentence = []
+  newsentence = sentence.split(" ")
   newsentance.each do |word|
   newsentance.push(word.reverse)
   end
